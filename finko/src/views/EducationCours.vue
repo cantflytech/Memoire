09000f4b5a90a3fe34aee5efbdebbb21c8c28e06
cantflytech <63524900+cantflytech@users.noreply.github.com>
2026-06-16 20:39:41 +0200

    Wrap education view in layout container
    Wrap FirstRows and EducationCours in a top-level div to provide consistent page layout (padding, background color, min height, flex column and spacing). This adds p-8, bg-[#F8FAFB], min-h-screen, flex flex-col and gap-10 to the view so the components are properly spaced and styled.

--- finko/src/views/EducationCours.vue
+++ finko/src/views/EducationCours.vue
@@ -4,6 +4,9 @@ import EducationCours from '@/components/Apprentissage/EducationDashboard.vue';
 </script>
 
 <template>
+   <div class="p-8 bg-[#F8FAFB] min-h-screen flex flex-col gap-10">
+    
   <FirstRows />
   <EducationCours />
+   </div>
 </template>
